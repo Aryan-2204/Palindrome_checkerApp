ac740e8df7efe98b94d17ce9074ea18e248439ee
unknown <akadam2204@gmail.com>
2026-02-26 15:25:17 +0530

    UC2 hardcoded pipeline

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -11,6 +11,22 @@ public class PalindromeCheckerApp {
             System.out.println("Application started successfully.");
             System.out.println("No palindrome logic implemented yet.");
             System.out.println("Startup flow established.");
+            // Hardcoded string
+            String input = "madam";
 
+            boolean isPalindrome = true;
+
+            // Loop only till half of the string length
+            for (int i = 0; i < input.length() / 2; i++) {
+
+                if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            // Display result
+            System.out.println("Input text: " + input);
+            System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
