487d9eba2717970accb2a0e6530fe1b86d681052
unknown <akadam2204@gmail.com>
2026-02-26 15:32:38 +0530

    UC3/palindrome checked

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -28,5 +28,22 @@ public class PalindromeCheckerApp {
             // Display result
             System.out.println("Input text: " + input);
             System.out.println("Is it a Palindrome? : " + isPalindrome);
+            // Original string
+            String original = "level";
+
+            // Variable to store reversed string
+            String reversed = "";
+
+            // Iterate the string in reverse order
+            for (int i = original.length() - 1; i >= 0; i--) {
+                reversed = reversed + original.charAt(i);
+            }
+
+            // Compare original and reversed strings
+            if (original.equals(reversed)) {
+                System.out.println("The word \"" + original + "\" is a Palindrome.");
+            } else {
+                System.out.println("The word \"" + original + "\" is NOT a Palindrome.");
+            }
     }
 }
